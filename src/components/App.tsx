cf826e71449dde13e1c4e55bbf04cc676fc8e9af
HernanFaifman <72212547+HernanFaifman@users.noreply.github.com>
2020-10-22 17:07:29 -0300

    [PRM-667] Google Analytics and Google Tag Manager integration (#17)
    * added google tag manager to app
    
    * added GA to App
    
    Co-authored-by: Hernan <hernan.faifman@gmail.com>

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,15 +1,30 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Amplify from "aws-amplify";
 import "antd/dist/antd.less";
-import "../assets/fonts/fontface.less";
 import { ThemeProvider } from "styled-components";
+import { Router, Route, Switch } from "react-router-dom";
+import Amplify from "aws-amplify";
+import TagManager from "react-gtm-module";
+import { createBrowserHistory } from "history";
+import ReactGA from "react-ga";
 import { theme } from "../constants/styles/theme";
-
 import Login from "../routes/login/login";
 import Authenticator from "./authenticator/authenticator";
 
 function App() {
+    const tagManagerId = {
+        gtmId: "GTM-TMP4C9Q",
+    };
+    TagManager.initialize(tagManagerId);
+
+    ReactGA.initialize(tagManagerId.gtmId);
+
+    const history = createBrowserHistory();
+
+    history.listen((location) => {
+        ReactGA.set({ page: location.pathname });
+        ReactGA.pageview(location.pathname);
+    });
+
     Amplify.configure({
         Auth: {
             region: process.env.REACT_APP_Auth_region,
@@ -24,7 +39,7 @@ function App() {
 
     return (
         <ThemeProvider theme={theme}>
-            <Router>
+            <Router history={history}>
                 <Switch>
                     <Route exact path="/" component={Login} />
                     <Authenticator>
