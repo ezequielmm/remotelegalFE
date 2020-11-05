5dc9f6ab4e7b4b4fddad3cd00f5b3161ec1150d5
HernanFaifman <72212547+HernanFaifman@users.noreply.github.com>
2020-11-05 09:25:02 -0300

    added location to useAuthentication dependency array (#71)
    Co-authored-by: Hernan <hernan.faifman@gmail.com>

--- src/hooks/useAuthentication.tsx
+++ src/hooks/useAuthentication.tsx
@@ -1,8 +1,10 @@
 import { useState, useEffect } from "react";
 import { Auth } from "aws-amplify";
+import { useLocation } from "react-router-dom";
 
 const useAuthentication = () => {
     const [isAuthenticated, setUserIsAuthenticated] = useState(null);
+    const location = useLocation();
     useEffect(() => {
         const checkAuthentication = async () => {
             try {
@@ -14,7 +16,7 @@ const useAuthentication = () => {
             }
         };
         checkAuthentication();
-    }, []);
+    }, [location]);
     return { isAuthenticated };
 };
 export default useAuthentication;
