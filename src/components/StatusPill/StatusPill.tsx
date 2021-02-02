9a3a3b1e4d36d728c30d9dd1b8a2766739bb7e66
dprieri-ms <67918978+dprieri-ms@users.noreply.github.com>
2021-02-02 15:40:57 -0300

    [PRM-1723] [UX-DEV] Status Pill dark theme (#329)
    * Dark theme for status pill
    
    * Add theme property
    
    * Add explicitly values for line-height and width
    
    * Add status enum

--- src/components/StatusPill/StatusPill.tsx
+++ src/components/StatusPill/StatusPill.tsx
@@ -1,42 +1,79 @@
 import React from "react";
 import styled from "styled-components";
 import { getREM, hexToRGBA } from "../../constants/styles/utils";
-import { theme } from "../../constants/styles/theme";
-import { ReactComponent as Completed } from "../../assets/icons/Success.svg";
+import { ReactComponent as Confirmed } from "../../assets/icons/Success.svg";
 import { ReactComponent as Pending } from "../../assets/icons/Pending.svg";
 import { ReactComponent as Canceled } from "../../assets/icons/Error.svg";
+import { ReactComponent as Completed } from "../../assets/icons/Checkmark.svg";
 import Icon from "../Icon";
+import { Theme, ThemeMode } from "../../types/ThemeType";
+
+export enum Status {
+    pending = "Pending",
+    approved = "Approved",
+    canceled = "Canceled",
+    confirmed = "Confirmed",
+}
 
 export interface IStatusPillProps {
-    status: "Completed" | "Pending" | "Canceled";
+    status: Status;
+    theme?: Theme;
 }
 
 const StyledPill = styled.span`
-    display: inline-flex;
-    align-items: center;
-    line-height: ${getREM(theme.default.spaces[9] + theme.default.spaces[1])};
-    padding: 0 ${getREM(theme.default.spaces[3])};
-    border-radius: ${getREM(theme.default.borderRadiusBase)};
-    font-size: ${getREM(theme.default.fontSizes[8])};
+    ${({ theme }) => {
+        const inDepoStyles = `
+            color: ${theme.colors.neutrals[6]};
+            &.Completed {
+                background-color: ${theme.colors.success[5]};
+            }
+            &.Confirmed {
+                background-color: ${theme.colors.inDepoNeutrals[9]};
+            }
+            &.Pending {
+                background-color: ${theme.colors.warning[5]};
+            }
+            &.Canceled {
+                background-color: ${theme.colors.error[5]};
+            }`;
+        const defaultStyles = `
+            &.Completed {
+                background-color: ${hexToRGBA(theme.colors.success[5], 0.1)};
+                color: ${theme.colors.success[5]};
+            }
+            &.Confirmed {
+                background-color: ${hexToRGBA(theme.colors.inDepoNeutrals[9], 0.1)};
+                color: ${theme.colors.inDepoNeutrals[9]};
+            }
+            &.Pending {
+                background-color: ${hexToRGBA(theme.colors.warning[5], 0.1)};
+                color: ${theme.colors.warning[5]};
+            }
+            &.Canceled {
+                background-color: ${hexToRGBA(theme.colors.error[5], 0.1)};
+                color: ${theme.colors.error[5]};
+            }`;
 
-    &.Completed {
-        background-color: ${hexToRGBA(theme.colors.success[5], 0.1)};
-        color: ${theme.colors.success[5]};
-    }
-    &.Pending {
-        background-color: ${hexToRGBA(theme.colors.warning[5], 0.1)};
-        color: ${theme.colors.warning[5]};
-    }
-    &.Canceled {
-        background-color: ${hexToRGBA(theme.colors.error[5], 0.1)};
-        color: ${theme.colors.error[5]};
-    }
+        const styles = `
+            display: flex;
+            align-items: center;
+            line-height: 1.625rem;
+            padding: 0 ${getREM(theme.default.spaces[3])};
+            border-radius: ${getREM(theme.default.borderRadiusBase)};
+            font-size: ${getREM(theme.default.fontSizes[8])};
+            width: 6rem;
 
-    .anticon {
-        margin-right: ${getREM(theme.default.spaces[2])};
-        font-size: ${getREM(theme.default.fontSizes[6])};
-        color: inherit;
-    }
+            .anticon {
+                margin-right: ${getREM(theme.default.spaces[2])};
+                font-size: ${getREM(theme.default.fontSizes[6])};
+                color: inherit;
+            }
+            
+            ${theme.mode === ThemeMode.inDepo ? inDepoStyles : defaultStyles};
+        `;
+
+        return styles;
+    }}
 `;
 
 const StatusPill = ({ status, ...rest }: IStatusPillProps) => {
@@ -45,6 +82,7 @@ const StatusPill = ({ status, ...rest }: IStatusPillProps) => {
             {
                 {
                     Completed: <Icon icon={Completed} />,
+                    Confirmed: <Icon icon={Confirmed} />,
                     Pending: <Icon icon={Pending} />,
                     Canceled: <Icon icon={Canceled} />,
                 }[status]
--- src/stories/StatusPill.stories.tsx
+++ src/stories/StatusPill.stories.tsx
@@ -1,6 +1,11 @@
 import React from "react";
 import { Story, Meta } from "@storybook/react/types-6-0";
 import StatusPill from "../components/StatusPill";
+import { ThemeMode } from "../types/ThemeType";
+import { theme } from "../constants/styles/theme";
+
+const darkTheme = { ...theme, mode: ThemeMode.inDepo };
+const lightTheme = { ...theme, mode: ThemeMode.default };
 
 export default {
     title: "Status Pill",
@@ -9,15 +14,24 @@ export default {
         status: {
             control: {
                 type: "select",
-                options: ["completed", "pending", "canceled"],
+                options: ["Completed", "Pending", "Canceled", "Confirmed"],
+            },
+        },
+        theme: {
+            control: {
+                type: "select",
+                options: ["Dark", "Light"],
             },
         },
     },
 } as Meta;
 
-const Template: Story = ({ ...args }) => <StatusPill {...args} status={args.status} />;
+const Template: Story = ({ ...args }) => {
+    const selectedTheme = args.theme === "Dark" ? darkTheme : lightTheme;
+    return <StatusPill {...args} status={args.status} theme={selectedTheme} />;
+};
 
 export const PRStatusPill = Template.bind({});
 PRStatusPill.args = {
-    status: "completed",
+    status: "Completed",
 };
