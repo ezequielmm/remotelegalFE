c6539474319e6fa68dd7dd1996f43fdea7666dd7
Franco Dalmasso <42449309+francomd@users.noreply.github.com>
2021-03-05 15:34:24 -0300

    Fix red button styles (#493)

--- src/routes/InDepo/Exhibits/ExhibitViewer/ExhibitViewerHeader/styles.tsx
+++ src/routes/InDepo/Exhibits/ExhibitViewer/ExhibitViewerHeader/styles.tsx
@@ -1,10 +1,29 @@
 import styled from "styled-components";
 import button from "../../../../../components/Button";
 
+// eslint-disable-next-line import/prefer-default-export
 export const StyledCloseButton = styled(button)`
-    background: ${({ theme }) => theme.colors.error[5]};
-    border: none;
-    &:hover {
-        background: ${({ theme }) => theme.colors.error[5]};
-    }
+    ${({ theme }) => {
+        const { error } = theme.colors;
+        const { textColorInverse } = theme.default;
+
+        return `
+            background: ${error[5]};
+            border-color: ${error[5]};
+            color: ${textColorInverse}; // TODO get contrast and return white or black text
+
+            &:hover,
+            &:focus {
+                background: ${error[4]};
+                border-color: ${error[4]};
+                color: ${textColorInverse}; // TODO get contrast and return white or black text
+            }
+
+            &:active {
+                background: ${error[6]};
+                border-color: ${error[6]};
+                color: ${textColorInverse}; // TODO get contrast and return white or black text
+            }
+        `;
+    }}
 `;
