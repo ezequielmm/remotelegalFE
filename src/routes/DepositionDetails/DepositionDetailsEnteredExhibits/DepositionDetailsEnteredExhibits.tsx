debd45d283bae5cae5d12413560c5a5a9b5e32de
Ale Paciotti <78363859+alepaciotti-ms@users.noreply.github.com>
2021-03-17 14:51:14 -0300

    [NO_TICKET] Hidde download button in entered exhibit (#548)
    * Hidde download button in entered exhibit
    
    * Remove unused components

--- src/routes/DepositionDetails/DepositionDetailsEnteredExhibits/DepositionDetailsEnteredExhibits.tsx
+++ src/routes/DepositionDetails/DepositionDetailsEnteredExhibits/DepositionDetailsEnteredExhibits.tsx
@@ -1,27 +1,27 @@
-import React, { useEffect, useState } from "react";
-import { RowSelectionType } from "antd/lib/table/interface.d";
+import React, { useEffect /* , useState */ } from "react";
+// import { RowSelectionType } from "antd/lib/table/interface.d";
 import Title from "../../../components/Typography/Title";
 import { DEPOSITION_DETAILS_ENTERED_EXHIBITS_TITLE } from "../../../constants/depositionDetails";
 import { useEnteredExhibit } from "../../../hooks/useEnteredExhibits";
-import Button from "../../../components/Button";
-import Icon from "../../../components/Icon";
-import { ReactComponent as DownloadIcon } from "../../../assets/icons/download.svg";
+// import Button from "../../../components/Button";
+// import Icon from "../../../components/Icon";
+// import { ReactComponent as DownloadIcon } from "../../../assets/icons/download.svg";
 
 import EnteredExhibitsTable from "./EnteredExhibitsTable";
 import { StyledEnteredContainer } from "./styles";
 import Space from "../../../components/Space";
-import * as CONSTANTS from "../../../constants/depositionDetails";
+// import * as CONSTANTS from "../../../constants/depositionDetails";
 
 export default function DepositionDetailsEnteredExhibits() {
     const { handleFetchFiles, enteredExhibits, enteredExhibitsPending } = useEnteredExhibit();
-    const [selectedRows, setSelectedRows] = useState([]);
-    const rowSelection = {
-        type: "checkbox" as RowSelectionType,
-        onChange: (selection: []) => {
-            setSelectedRows(selection);
-        },
-    };
-    const isDownloadDisabled = !selectedRows.length;
+    // const [selectedRows, setSelectedRows] = useState([]);
+    // const rowSelection = {
+    //     type: "checkbox" as RowSelectionType,
+    //     onChange: (selection: []) => {
+    //         setSelectedRows(selection);
+    //     },
+    // };
+    // const isDownloadDisabled = !selectedRows.length;
 
     useEffect(() => {
         handleFetchFiles();
@@ -35,19 +35,19 @@ export default function DepositionDetailsEnteredExhibits() {
                         {DEPOSITION_DETAILS_ENTERED_EXHIBITS_TITLE}
                     </Title>
                 </Space.Item>
-                <Button
+                {/* <Button
                     type="primary"
                     icon={<Icon icon={DownloadIcon} size={9} />}
                     size="small"
                     disabled={isDownloadDisabled}
                 >
                     {CONSTANTS.DETAILS_TRANSCRIPT_BUTTON_DOWNLOAD}
-                </Button>
+                </Button> */}
             </Space>
             <EnteredExhibitsTable
                 loading={enteredExhibitsPending}
                 dataSource={enteredExhibits || []}
-                rowSelection={rowSelection}
+                // rowSelection={rowSelection}
             />
         </StyledEnteredContainer>
     );
