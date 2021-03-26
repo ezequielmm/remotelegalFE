598bf247fd64c78f5155ce95055c03f747837189
pmoyano-ms <72083504+pmoyano-ms@users.noreply.github.com>
2021-03-26 10:36:57 -0300

    Fix issue when the current room state has not localparticipant (#588)

--- src/routes/InDepo/InDepo.tsx
+++ src/routes/InDepo/InDepo.tsx
@@ -120,9 +120,9 @@ const InDepo = () => {
     }, [currentExhibit]);
 
     React.useEffect(() => {
-        if (participants.length && currentRoom.localParticipant) {
-            const localParticipantEmail = JSON.parse(currentRoom?.localParticipant.identity).email;
-            const isMuted = participants.find((participant) => participant?.email === localParticipantEmail).isMuted;
+        if (participants.length && currentRoom?.localParticipant) {
+            const localParticipantEmail = JSON.parse(currentRoom?.localParticipant?.identity)?.email;
+            const isMuted = participants.find((participant) => participant?.email === localParticipantEmail)?.isMuted;
             setInitialAudioEnabled(!isMuted);
         }
     }, [participants, currentRoom]);
