61ded1e19bbe3308ad0d81a1a74f3205b4f941b7
arieltapia16 <arieltapia16@gmail.com>
2021-08-02 15:06:51 -0300

    [PRM-3852]: Added versions bump (#991)
    * [PRM-3852]: Added versions bump
    
    * [PRM-3852] Update Github workflows for version bumping
    
    * [PRM-3852]: Removed unless code
    
    Co-authored-by: Ariel Tapia <ariel@learnfully.com>
    Co-authored-by: Ezequiel Szturmaj <eszturmaj@makingsense.com>

---
+++ generate-build-version.js
@@ -0,0 +1,21 @@
+const fs = require("fs");
+const packageJson = require("./package.json");
+
+const appVersion = packageJson.version;
+
+const jsonData = {
+    version: appVersion,
+};
+
+const jsonContent = JSON.stringify(jsonData);
+
+// eslint-disable-next-line func-names
+// eslint-disable-next-line consistent-return
+fs.writeFile("./public/meta.json", jsonContent, "utf8", (err) => {
+    if (err) {
+        console.log("An error occured while writing JSON Object to meta.json");
+        return console.log(err);
+    }
+
+    console.log("meta.json file has been saved with latest version number");
+});
