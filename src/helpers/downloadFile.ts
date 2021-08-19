2dce2475e23b7a9e7ce95f378ad57f71424a6326
pmoyano-ms <72083504+pmoyano-ms@users.noreply.github.com>
2021-08-19 11:28:24 -0300

    [PRM-4084] - Fix Download attribute not working iPad (#1043)
    * use blob from url to avoid CORS issues
    
    * skip broken ut
    
    * Add UTs
    
    Co-authored-by: Pedro Moyano <pmoyano@directly.com>

--- src/helpers/downloadFile.ts
+++ src/helpers/downloadFile.ts
@@ -1,12 +1,19 @@
-const downloadFile = (fileUrl: string) => {
-    const link = document.createElement("a");
-    link.href = fileUrl;
-    document.body.appendChild(link);
-    link.setAttribute("download", "download");
-    link.setAttribute("target", "_blank");
-    setTimeout(() => {
-        link.click();
-        document.body.removeChild(link);
-    }, 500);
+const downloadFile = async (fileUrl: string, onError?: () => void) => {
+    try {
+        const blob = await fetch(fileUrl).then((r) => r.blob());
+        const url = window.URL.createObjectURL(new Blob([blob]));
+        const link = document.createElement("a");
+        link.href = url;
+        document.body.appendChild(link);
+        link.setAttribute("download", fileUrl.split("/").pop().split("?")[0]);
+        link.setAttribute("target", "_blank");
+        setTimeout(() => {
+            link.click();
+            document.body.removeChild(link);
+        }, 500);
+    } catch (e) {
+        console.error("downloadFile error:", e);
+        if (onError) return onError();
+    }
 };
 export default downloadFile;
