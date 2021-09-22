5e93de0fe8fe025170548d48f7e818c38e02099f
HernanFaifman <72212547+HernanFaifman@users.noreply.github.com>
2021-09-22 11:33:38 -0300

    No ticket fix build (#1127)
    * Fixed build
    
    * Readded download function to downloadFile

--- src/helpers/downloadFile.ts
+++ src/helpers/downloadFile.ts
@@ -1,5 +1,18 @@
-const downloadFile = async (fileUrl: string, fileName?: string) => {
+export enum DownloadStatus {
+    "pending",
+    "completed",
+    "error",
+}
+
+export type DownloadStatusType = keyof typeof DownloadStatus;
+
+const downloadFile = async (
+    fileUrl: string,
+    fileName?: string,
+    onDownloadStatus?: (status: DownloadStatusType) => void
+) => {
     let url = fileUrl;
+    if (onDownloadStatus) onDownloadStatus("pending");
     const params = new URLSearchParams(url);
     const queryParamFilename = params.get("response-content-disposition");
     const queryParamsFilenameString = queryParamFilename?.substring(queryParamFilename.indexOf("=") + 1);
@@ -7,12 +20,13 @@ const downloadFile = async (fileUrl: string, fileName?: string) => {
         const blob = await fetch(fileUrl).then((r) => r.blob());
         url = window.URL.createObjectURL(new Blob([blob]));
     } catch (e) {
+        if (onDownloadStatus) onDownloadStatus("error");
         console.error("downloadFile error:", e);
     }
+    if (onDownloadStatus) onDownloadStatus("completed");
     const link = document.createElement("a");
     link.href = url;
     document.body.appendChild(link);
-
     link.setAttribute("target", "_blank");
     link.setAttribute("download", fileName || queryParamsFilenameString || fileUrl.split("/").pop().split("?")[0]);
     setTimeout(() => {
