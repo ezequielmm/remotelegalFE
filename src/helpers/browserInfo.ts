5380fdd586c63f6f87ebab8848c149a5ffd66536
Gonzalo Romero <71275081+gromero-ms@users.noreply.github.com>
2021-10-14 12:19:32 -0300

    [PRM-4610] Fix Browser version
    Update to fullBrowserVersion in browserInfo.ts

--- src/helpers/browserInfo.ts
+++ src/helpers/browserInfo.ts
@@ -2,7 +2,7 @@ import * as deviceInfo from "react-device-detect";
 
 const getBrowserInfo = (): { browser: string; device: string; OS: string } => {
     return {
-        browser: `${deviceInfo.browserName} ${deviceInfo.browserVersion}`,
+        browser: `${deviceInfo.browserName} ${deviceInfo.fullBrowserVersion}`,
         device: `${deviceInfo.deviceType === "browser" ? "desktop" : deviceInfo.deviceType}`,
         OS: `${deviceInfo.osName} ${deviceInfo.osVersion}`,
     };
